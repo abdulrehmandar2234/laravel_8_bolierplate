a6c969772630f382bd95179bb0314fd256255cbe
AbdulRehman Dar <dar-abdul-rehman@ashlarglobal.com>
2021-04-30 22:58:22 +0500

    removed extra seeders from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(CurrenciesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(ProductNodesTableSeeder::class);
-        $this->call(WebsitesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(CategoryLinksTableSeeder::class);
-        $this->call(SlidersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);               
     }
 }
